cb521e08662b2cb8f2b33a95eff39492eb092770
Alejandro Ramírez <81694980+lexrammart@users.noreply.github.com>
2026-03-15 01:29:19 -0600

    fix: resolucion de parsing en tags y parche ssl para pyinstaller

--- core/updater.py
+++ core/updater.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 import ssl
 
-ACTUAL_VERSION = "1.1.5"
+ACTUAL_VERSION = "1.1.7"
 
 
 def check_update():
@@ -10,36 +10,41 @@ def check_update():
     Consulta la API pública de GitHub para verificar si hay un nuevo Release.
     Retorna una tupla (True, dict_datos) si hay actualización, o None si estás al día.
     """
-    # Esta es la API oficial de GitHub apuntando a tu repo público
     url = "https://api.github.com/repos/lexrammart/MATI-Releases/releases/latest"
 
     try:
-        # 1. EL PARCHE SSL: Creamos un contexto que ignora la verificación del certificado
+        # 1. PARCHE SSL
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
 
-        # Hacemos la petición (simulando ser un navegador básico para que GitHub no nos bloquee)
         req = urllib.request.Request(url, headers={"User-Agent": "MATI-Updater"})
 
-        # Le pasamos el contexto SSL modificado a la petición
         with urllib.request.urlopen(req, timeout=5, context=ctx) as response:
             data = json.loads(response.read().decode())
 
-        # GitHub guarda la versión en "tag_name" (ej. "v1.1.5")
         latest_version_tag = data.get("tag_name", "")
 
-        # Le quitamos la 'v' y espacios fantasma
-        latest_version = latest_version_tag.replace("v", "").strip()
+        # CHISMOSAFE: Imprimimos qué diablos nos mandó GitHub
+        print(f"[*] INFO: GitHub dice que la última versión es: '{latest_version_tag}'")
 
-        # 2. EL PARCHE MATEMÁTICO: Convertimos "1.1.5" a (1, 1, 5) para comparar exacto
-        version_github = tuple(map(int, latest_version.split(".")))
-        version_local = tuple(map(int, ACTUAL_VERSION.split(".")))
+        latest_version = latest_version_tag.replace("v", "").strip(" .")
+        if not latest_version:
+            print("Advertencia: El tag llegó completamente vacío.")
+            return None
 
-        # El cuerpo del Release en GitHub nos servirá como changelog
+        # 2. PARCHE MATEMÁTICO CON BLINDAJE EXTRA
+        try:
+            version_github = tuple(map(int, latest_version.split(".")))
+            version_local = tuple(map(int, ACTUAL_VERSION.split(".")))
+        except ValueError:
+            print(
+                f"Error matemático: El tag '{latest_version_tag}' tiene un formato que no se puede comparar."
+            )
+            return None
+
         changelog = data.get("body", "Mejoras de rendimiento y telemetría.")
 
-        # Si la versión de GitHub es mayor a la tuya, disparamos la alerta
         if version_github > version_local:
             datos_reales = {"version": latest_version, "changelog": changelog}
             return (True, datos_reales)
